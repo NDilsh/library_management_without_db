d5fb5585c1dc51f25cbf6792468e4ef3e4bf78c4
NDilsh <nadun31dilshan@gmail.com>
2023-07-15 19:32:27 +0530

    update models.py

--- book_manager/models.py
+++ book_manager/models.py
@@ -6,7 +6,7 @@ class Book(models.Model):
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=150, default='anonymous')
     genre = models.CharField(max_length=100)
-    height = models.IntegerField()
+    height = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
     # Add more fields as required
 
