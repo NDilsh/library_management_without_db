40f350506d08cf032b5ee231aa4df861edf06aed
NDilsh <nadun31dilshan@gmail.com>
2023-07-15 12:29:59 +0530

    Create Models

--- book_manager/models.py
+++ book_manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    author = models.CharField(max_length=150, default='anonymous')
+    genre = models.CharField(max_length=100)
+    height = models.IntegerField()
+    publisher = models.CharField(max_length=100)
+    # Add more fields as required
+
+    def __str__(self):
+        return self.title
