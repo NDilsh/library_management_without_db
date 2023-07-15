f5dec9663163cf8757207c5e1d2976c530cfe84a
NDilsh <nadun31dilshan@gmail.com>
2023-07-15 13:20:35 +0530

    Create views file

--- book_manager/views.py
+++ book_manager/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from .models import Book
+
 
 # Create your views here.
+def book_list(request):
+    query = request.GET.get('q')
+
+    if query:
+        books = Book.objects.filter(Q(title__icontains=query) | Q(genre__icontains=query))
+    else:
+        books = Book.objects.all()
+    return render(request, 'book_manager/book_list.html', {'books': books})
+
+
+def add_book(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        genre = request.POST['genre']
+        height = request.POST['height']
+        publisher = request.POST['publisher']
+        Book.objects.create(title=title, author=author, genre=genre, height=height, publisher=publisher)
+        return redirect('book_list')
+    return render(request, 'book_manager/add_book.html')
+
+
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.genre = request.POST['genre']
+        book.height = request.POST['height']
+        book.publisher = request.POST['publisher']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'book_manager/edit_book.html', {'book': book})
+
+
+def delete_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    book.delete()
+    return redirect('book_list')
